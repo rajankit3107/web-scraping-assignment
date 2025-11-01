0c825e571040128cc02d724b33399ecc6b9d1840
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-11-01 22:41:45 +0530

    chore : added required types

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export type JiraIssue = {
+   id: string;
+   key: string;
+   fields: {
+      summary: string;
+      description: string;
+      status: { name: string };
+      project: { key: string };
+      reporter?: { displayName: string };
+      assignee?: { displayName: string };
+      created: Date;
+      updated: Date;
+      comment: { comments: { body: string[] } };
+   };
+};
+
+export type JiraResponse = {
+   issues: JiraIssue[];
+   maxResults: number;
+};
