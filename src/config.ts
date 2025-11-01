d24282d9a01cbfb9a25b4ba9b693465b534d28b5
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-11-01 22:37:37 +0530

    chore : added config files

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const PROJECTS = ['HADOOP', 'SPARK', 'KAFKA']; // 3 Apache projects
+export const BASE_URL = 'https://issues.apache.org/jira/rest/api/2';
+export const OUTPUT_DIR = './data';
+export const PAGE_SIZE = 50;
+export const MAX_RETRIES = 5;
+export const REQUEST_TIMEOUT = 10000;
